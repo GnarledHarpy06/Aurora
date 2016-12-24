4605ded952d1f82b0fe4325200248db1741dd906
GnarledHarpy06 <https://github.com/GnarledHarpy06/>
2016-12-25 00:35:14 +0700

    py style

--- Project-Aurora/Scripts/Devices/example_py_script.py
+++ Project-Aurora/Scripts/Devices/example_py_script.py
@@ -14,42 +14,43 @@ class main():
     devicename = "Example Python Device Script"
     enabled = False  # Switch to True, to enable it in Aurora
     device_color = Color.Black
-    
+
     def Initialize(self):
         try:
             # Perform necessary actions to initialize your device
             return True
         except:
             return False
-    
+
     def Reset(self):
         # Perform necessary actions to reset your device
         return
-        
+
     def Shutdown(self):
         # Perform necessary actions to shutdown your device
         return
-    
+
     def UpdateDevice(self, keyColors, forced):
         try:
             for kvp in keyColors:
                 # Iterate over each key and color and send them to your device
 
                 if kvp.Key == DeviceKeys.Peripheral:
-                    # For example if we're basing our device color on Peripheral colors
+                    # For example if we're basing our device color
+					# on Peripheral colors
                     self.SendColorToDevice(kvp.Value, forced)
 
             return True
         except Exception as e:
             Global.logger.LogLine("[PY Script] Exception: "+str(e))
-            
+
             return False
 
     def CompareColors(self, color1, color2):
-        if color1.A == color2.A and 
-		   color1.R == color2.R and
-		   color1.G == color2.G and 
-		   color1.B == color2.B:
+        if (color1.A == color2.A and
+            color1.R == color2.R and
+            color1.G == color2.G and
+            color1.B == color2.B):
             return True
         else:
             return False
@@ -63,7 +64,7 @@ class main():
 			#       Only use logging for debugging
             Global.logger.LogLine("[PY Script] Sent a color, "
 				+ str(color) + " to the device")
-            
+
             # Update device color locally
             self.device_color = color
         return
--- Script Examples/example_py_script.py
+++ Script Examples/example_py_script.py
@@ -11,27 +11,28 @@ clr.AddReference("System.Drawing")
 
 array_device_keys = System.Array[DeviceKeys]
 
+
 class main():
     ID = "ExamplePyEffect"
     ForegroundColour = Color.Red
     BackgroundColour = Color.Black
     DefaultKeys = KeySequence(
-		array_device_keys((DeviceKeys.TAB,
-						   DeviceKeys.Q,
-						   DeviceKeys.W,
-						   DeviceKeys.E,
-						   DeviceKeys.R,
-						   DeviceKeys.T,
-						   DeviceKeys.Y,
-						   DeviceKeys.U,
-						   DeviceKeys.I,
-						   DeviceKeys.O,
-						   DeviceKeys.P)))
+        array_device_keys((DeviceKeys.TAB,
+                           DeviceKeys.Q,
+                           DeviceKeys.W,
+                           DeviceKeys.E,
+                           DeviceKeys.R,
+                           DeviceKeys.T,
+                           DeviceKeys.Y,
+                           DeviceKeys.U,
+                           DeviceKeys.I,
+                           DeviceKeys.O,
+                           DeviceKeys.P)))
     def UpdateLights(self, settings, state):
         layer = EffectLayer(self.ID)
         layer.PercentEffect(self.ForegroundColour,
-							self.BackgroundColour,
-							settings.Keys,
-							System.DateTime.Now.Second,
-							60);
+                            self.BackgroundColour,
+                            settings.Keys,
+                            System.DateTime.Now.Second,
+                            60);
         return layer
